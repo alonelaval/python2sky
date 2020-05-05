c558584aeec83b002e381c9620ccfe530b7b102c
huawei <huawei@bit-s.cn>
2020-05-05 17:54:27 +0800

    添加样例和文档

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+## python2sky 
 
-尝试写一个 python 的客户端
+python2sky 是一个python 语言的 skywalking 的客户端，遵循了  [Apache SkyWalking](https://github.com/apache/incubator-skywalking)  的tracing标准格式，现阶段支持的服务端版本为v2的协议。
+
+### 安装：
+
+```
+  pip install python2sky
+```
+
+该项目花了大概3天的时间，后续会继续改进，在skywalking服务端 为6.6的时候测试通过，如下图：
+
+
+
+
+
+
+##  快速入门：
+在测试 agent 的时候，使用了 flask，手动埋点的方式，后续会改进成自动埋点，请查看[样例](https://github.com/alonelaval/python2sky/blob/master/tests/falsk_test/test_flask.py) 
---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+# author：huawei
+
+
+from setuptools import setup, find_packages
+
+
+
+
+packages = ['python2sky']
+
+requires =["grpcio==1.28.1","grpcio-tools==1.28.1"]
+
+test_requirements = [
+    'pytest-httpbin==0.0.7',
+    'pytest-cov',
+    'pytest-mock',
+    'pytest-xdist',
+    'PySocks>=1.5.6, !=1.5.7',
+    'pytest>=3'
+]
+
+setup(
+    name = "python2sky",
+    version = "0.1.0",
+    keywords = ("skywalking","agent","skywalking aget","apm","trace","distributed trace"),
+    description = "skywalking agent for python",
+    long_description = "apm agent",
+    license = "apache Licence",
+
+    url = "https://github.com/alonelaval/python2sky",
+    author = "huawei",
+    author_email = "120huawei@163.com",
+
+    packages = packages,
+    include_package_data = True,
+    platforms = "any",
+    install_requires = requires,
+
+
+)
